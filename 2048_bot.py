15cea6a60cdc8616dd2f7e56071d9893c940a273
sudo-style <87545795+sudo-style@users.noreply.github.com>
2024-11-25 17:48:47 -0500

    Added logic for merging rows/cols
    I added some test cases, and it seems that all of them passed. Should be able to work with any direction of movements, might need to be reversed, for each row, or col.
    
    Going to add it to the class

--- 2048_bot.py
+++ 2048_bot.py
@@ -1,3 +1,8 @@
+# colorize the terminal
+import colorama 
+from colorama import Fore, Back, Style
+
+
 import numpy as np
 
 SIZE = 4
@@ -44,14 +49,73 @@ class Game:
             self.move_down()
         elif direction == 'd':
             self.move_right()
+        else: 
+            print('invalid move')
+            self.print_board()
+            return
+        self.add_new_tile()
+        self.print_board()
 
-def main():
+
+'''def main():
     game = Game()
     game.print_board()
 
     while not game.isGameOver:
         move = input('next move: ')
-        game.move(move)
+        game.move(move)'''
+
+
+
+
+def slide_and_merge_row(row):
+    # get all non-zero values
+    # [2, 4, 0, 2] -> [2, 4, 2]
+    # [0, 0, 0, 2] -> [2]
+    non_zero = [value for value in row if value != 0]
+    
+    # keep track of which values have been merged
+    
+    # [False, False, False] means no values have been merged
+    # [True, False, False] means the first value has been merged
+    merged = False
+    
+    new_row = []
+    for i, value in enumerate(non_zero):
+        # if the value has been merged, skip it
+        if merged: 
+            merged = False
+            continue
+        
+        # if the current value is the same as the next value, merge them
+        if i < len(non_zero) - 1 and value == non_zero[i + 1] and not merged:
+            new_row.append(value * 2)
+            merged = True
+        
+        # if not able to merge, just add the value
+        else: new_row.append(value)
+
+    # add zeros for the remaining empty cells
+    # [2, 4, 2] -> [2, 4, 2, 0]
+    # [2] -> [2, 0, 0, 0]
+    new_row += [0] * (SIZE - len(new_row))
+    return new_row
+
+def main():
+    test_case = []
+    test_case.append([[0, 0, 0, 2], [2, 0, 0, 0]])
+    test_case.append([[2, 0, 0, 2], [4, 0, 0, 0]])
+    test_case.append([[2, 0, 2, 2], [4, 2, 0, 0]])
+    test_case.append([[2, 2, 2, 2], [4, 4, 0, 0]])
+    test_case.append([[2, 2, 2, 4], [4, 2, 4, 0]])
+
+    for i, (input, expected) in enumerate(test_case):
+        result = slide_and_merge_row(input)
+        if result == expected:
+            print(f'{Fore.GREEN}in: {input} out: {result} exp: {expected} case {i + 1} Passed')
+        else:
+            print(f'{Fore.RED  }in: {input} out: {result} {Fore.GREEN}exp: {expected}{Fore.RESET} {Fore.RED}case {i + 1} Failed')
+        print(f'{Fore.RESET}')
 
 if __name__ == '__main__':
     main()
