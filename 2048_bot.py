4b75eb687f3d21bb17213d355fd416c236f481d8
sudo-style <87545795+sudo-style@users.noreply.github.com>
2024-11-25 18:03:38 -0500

    Up moves working
    Added a stage, were it checks for invalid moves, by seeing if there is a change from the previous board state. Next going to do the same for all movements

--- 2048_bot.py
+++ 2048_bot.py
@@ -1,9 +1,8 @@
+import numpy as np
+
 # colorize the terminal
 import colorama 
 from colorama import Fore, Back, Style
-
-
-import numpy as np
 
 SIZE = 4
 
@@ -28,9 +27,53 @@ class Game:
     def isGameOver(self):
         return self.isGameOver
     
-    def move_up(self):
-        print('up')
+    def slide_and_merge_row(self, row):
+        # get all non-zero values
+        # [2, 4, 0, 2] -> [2, 4, 2]
+        # [0, 0, 0, 2] -> [2]
+        non_zero = [value for value in row if value != 0]
+        
+        # keep track of which values have been merged
+        
+        # [False, False, False] means no values have been merged
+        # [True, False, False] means the first value has been merged
+        merged = False
+        
+        new_row = []
+        for i, value in enumerate(non_zero):
+            # if the value has been merged, skip it
+            if merged: 
+                merged = False
+                continue
+            
+            # if the current value is the same as the next value, merge them
+            if i < len(non_zero) - 1 and value == non_zero[i + 1] and not merged:
+                new_row.append(value * 2)
+                merged = True
+            
+            # if not able to merge, just add the value
+            else: new_row.append(value)
 
+        # add zeros for the remaining empty cells
+        # [2, 4, 2] -> [2, 4, 2, 0]
+        # [2] -> [2, 0, 0, 0]
+        new_row += [0] * (SIZE - len(new_row))
+        return new_row
+    
+    def prev_board_same(self, prev_board):
+        return np.array_equal(self.board, prev_board)
+    
+    def move_up(self):
+        new_board = self.board.copy()
+        
+        # for every column in the board, slide and merge
+        for c in range(SIZE):
+            column = [new_board[r][c] for r in range(SIZE)]
+            new_column = self.slide_and_merge_row(column)
+            for r in range(SIZE):
+                new_board[r][c] = new_column[r]
+        return new_board
+    
     def move_down(self):
         print('down')
 
@@ -42,80 +85,32 @@ class Game:
     
     def move(self, direction):
         if direction == 'w':
-            self.move_up()
+            board = self.move_up()
         elif direction == 'a':
-            self.move_left()
+            board = self.move_left()
         elif direction == 's':
-            self.move_down()
+            board = self.move_down()
         elif direction == 'd':
-            self.move_right()
+            board = self.move_right()
         else: 
             print('invalid move')
             self.print_board()
             return
-        self.add_new_tile()
-        self.print_board()
+        if self.prev_board_same(board):
+            print('invalid move')
+            self.print_board()
+        else:
+            self.board = board
+            self.add_new_tile()
+            self.print_board()
 
-
-'''def main():
+def main():
     game = Game()
     game.print_board()
 
     while not game.isGameOver:
         move = input('next move: ')
-        game.move(move)'''
-
-
-
-
-def slide_and_merge_row(row):
-    # get all non-zero values
-    # [2, 4, 0, 2] -> [2, 4, 2]
-    # [0, 0, 0, 2] -> [2]
-    non_zero = [value for value in row if value != 0]
-    
-    # keep track of which values have been merged
-    
-    # [False, False, False] means no values have been merged
-    # [True, False, False] means the first value has been merged
-    merged = False
-    
-    new_row = []
-    for i, value in enumerate(non_zero):
-        # if the value has been merged, skip it
-        if merged: 
-            merged = False
-            continue
-        
-        # if the current value is the same as the next value, merge them
-        if i < len(non_zero) - 1 and value == non_zero[i + 1] and not merged:
-            new_row.append(value * 2)
-            merged = True
-        
-        # if not able to merge, just add the value
-        else: new_row.append(value)
-
-    # add zeros for the remaining empty cells
-    # [2, 4, 2] -> [2, 4, 2, 0]
-    # [2] -> [2, 0, 0, 0]
-    new_row += [0] * (SIZE - len(new_row))
-    return new_row
-
-def main():
-    test_case = []
-    test_case.append([[0, 0, 0, 2], [2, 0, 0, 0]])
-    test_case.append([[2, 0, 0, 2], [4, 0, 0, 0]])
-    test_case.append([[2, 0, 2, 2], [4, 2, 0, 0]])
-    test_case.append([[2, 2, 2, 2], [4, 4, 0, 0]])
-    test_case.append([[2, 2, 2, 4], [4, 2, 4, 0]])
-
-    for i, (input, expected) in enumerate(test_case):
-        result = slide_and_merge_row(input)
-        if result == expected:
-            print(f'{Fore.GREEN}in: {input} out: {result} exp: {expected} case {i + 1} Passed')
-        else:
-            print(f'{Fore.RED  }in: {input} out: {result} {Fore.GREEN}exp: {expected}{Fore.RESET} {Fore.RED}case {i + 1} Failed')
-        print(f'{Fore.RESET}')
+        game.move(move)
 
 if __name__ == '__main__':
     main()
