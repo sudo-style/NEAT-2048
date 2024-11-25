752ec8545118a054c6c15e86e7be64becaa0cf06
sudo-style <87545795+sudo-style@users.noreply.github.com>
2024-11-25 15:43:02 -0500

    getting input: left, down, up, right
    Next going to change the boardstate

--- 2048_bot.py
+++ 2048_bot.py
@@ -6,6 +6,7 @@ class Game:
     def __init__(self):
         self.board = np.zeros((SIZE, SIZE), dtype=int)
         self.add_new_tile()
+        self.isGameOver = False
 
     def random_value(self):
         return np.random.choice([2, 4], p=[0.9, 0.1])
@@ -19,9 +20,38 @@ class Game:
     def print_board(self):
         print(self.board)
 
+    def isGameOver(self):
+        return self.isGameOver
+    
+    def move_up(self):
+        print('up')
+
+    def move_down(self):
+        print('down')
+
+    def move_left(self):
+        print('left')
+
+    def move_right(self):
+        print('right')
+    
+    def move(self, direction):
+        if direction == 'w':
+            self.move_up()
+        elif direction == 'a':
+            self.move_left()
+        elif direction == 's':
+            self.move_down()
+        elif direction == 'd':
+            self.move_right()
+
 def main():
     game = Game()
     game.print_board()
+
+    while not game.isGameOver:
+        move = input('next move: ')
+        game.move(move)
 
 if __name__ == '__main__':
     main()
