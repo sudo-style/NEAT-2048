5b70cc207d64917149d9e26cb71a077b19d37332
sudo-style <87545795+sudo-style@users.noreply.github.com>
2024-11-26 13:02:40 -0500

    Movement system working, and changed board state
    Changed the board state, now using 2^n for printing, n for the board state. 
    I think this will be easier for the neural network to learn rather than using the real big numbers that scale exponentially.

--- 2048_bot.py
+++ 2048_bot.py
@@ -11,18 +11,21 @@ class Game:
         self.board = np.zeros((SIZE, SIZE), dtype=int)
         self.add_new_tile()
         self.isGameOver = False
+        self.moves = 0
 
     def random_value(self):
-        return np.random.choice([2, 4], p=[0.9, 0.1])
+        return np.random.choice([1, 2], p=[0.9, 0.1])
 
     def add_new_tile(self):
+        # get all empty cells
         empty_cells = [(r, c) for r in range(SIZE) for c in range(SIZE) if self.board[r][c] == 0]
         if len(empty_cells) > 0:
+            # randomly choose an empty cell
             r, c = empty_cells[np.random.randint(len(empty_cells))]
             self.board[r][c] = self.random_value()
 
     def print_board(self):
-        print(self.board)
+        print(2**self.board)
 
     def isGameOver(self):
         return self.isGameOver
@@ -48,7 +51,7 @@ class Game:
             
             # if the current value is the same as the next value, merge them
             if i < len(non_zero) - 1 and value == non_zero[i + 1] and not merged:
-                new_row.append(value * 2)
+                new_row.append(value + 1)
                 merged = True
             
             # if not able to merge, just add the value
@@ -62,40 +65,40 @@ class Game:
     
     def prev_board_same(self, prev_board):
         return np.array_equal(self.board, prev_board)
-    
-    def move_up(self):
-        new_board = self.board.copy()
+
+    def slide_and_merge(self, direction):
+        board = self.board.copy()
         
-        # for every column in the board, slide and merge
-        for c in range(SIZE):
-            column = [new_board[r][c] for r in range(SIZE)]
-            new_column = self.slide_and_merge_row(column)
+        if direction in ('LEFT', 'RIGHT'):
             for r in range(SIZE):
-                new_board[r][c] = new_column[r]
-        return new_board
-    
-    def move_down(self):
-        print('down')
-
-    def move_left(self):
-        print('left')
-
-    def move_right(self):
-        print('right')
-    
+                row = board[r]
+                if direction == 'RIGHT':
+                    row = row[::-1]
+                new_row = self.slide_and_merge_row(row)
+                if direction == 'RIGHT':
+                    new_row.reverse()
+                board[r] = new_row
+        elif direction in ('UP', 'DOWN'):
+            for c in range(SIZE):
+                column = [board[r][c] for r in range(SIZE)]
+                if direction == 'DOWN':
+                    # reverse the column
+                    column = column[::-1]
+                new_column = self.slide_and_merge_row(column)
+                if direction == 'DOWN':
+                    new_column.reverse()
+                for r in range(SIZE):
+                    board[r][c] = new_column[r]
+        return board
+            
     def move(self, direction):
-        if direction == 'w':
-            board = self.move_up()
-        elif direction == 'a':
-            board = self.move_left()
-        elif direction == 's':
-            board = self.move_down()
-        elif direction == 'd':
-            board = self.move_right()
-        else: 
+        if direction not in ('UP', 'DOWN', 'LEFT', 'RIGHT'):
             print('invalid move')
             self.print_board()
             return
+
+        board = self.slide_and_merge(direction)
+    
         if self.prev_board_same(board):
             print('invalid move')
             self.print_board()
@@ -103,14 +106,26 @@ class Game:
             self.board = board
             self.add_new_tile()
             self.print_board()
+            self.moves += 1 # this will determine the fitness of the bot
+
+    def setBoard(self, board):
+        self.board = board
 
 def main():
     game = Game()
     game.print_board()
 
+    directions = {'w': 'UP', 'a': 'LEFT', 's': 'DOWN', 'd': 'RIGHT'}
+
     while not game.isGameOver:
         move = input('next move: ')
-        game.move(move)
+        while move not in directions:
+            game.print_board()
+            move = input('try again: ')
+            
+
+        direction = directions[move]
+        game.move(direction)
 
 if __name__ == '__main__':
     main()
